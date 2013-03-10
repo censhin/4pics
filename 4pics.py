a3d3b7473c65974a76d27f54e128fb02d5cccb3c
censhin <censhin@gmail.com>
2013-03-10 15:47:56 +0000

    fixed indentation

--- 4pics.py
+++ 4pics.py
@@ -6,51 +6,51 @@ import re
 import threading
 
 def get_images(url):
-	r = re.compile('//images.4chan.org/[^"]*(?:.jpg|.jpeg|.bmp|.gif|.png)')
-	return r.findall(urllib2.urlopen(url).read())
+    r = re.compile('//images.4chan.org/[^"]*(?:.jpg|.jpeg|.bmp|.gif|.png)')
+    return r.findall(urllib2.urlopen(url).read())
 
 def download(url, folder = ''):
-	web_file = urllib2.urlopen(url)
-	if folder != '' and folder[-1] != '/':
-		folder += '/'
-	local_file = open(folder + url.split('/')[-1], 'w')
-	local_file.write(web_file.read())
-	web_file.close()
-	local_file.close()
+    web_file = urllib2.urlopen(url)
+    if folder != '' and folder[-1] != '/':
+        folder += '/'
+    local_file = open(folder + url.split('/')[-1], 'w')
+    local_file.write(web_file.read())
+    web_file.close()
+    local_file.close()
 
 class Download(threading.Thread):
-	def __init__(self, url, folder = ''):
-		threading.Thread.__init__(self)
-		self.url = url
-		self.folder = folder
-	def run(self):
-		download(self.url, self.folder)
+    def __init__(self, url, folder = ''):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.folder = folder
+    def run(self):
+        download(self.url, self.folder)
 
 if __name__ == '__main__':
-	if len(sys.argv) == 2 or len(sys.argv) == 3:
-			list = get_images(sys.argv[1])
-			done = []
-			threads = []
+    if len(sys.argv) == 2 or len(sys.argv) == 3:
+        list = get_images(sys.argv[1])
+        done = []
+        threads = []
 
-			for i in list:
-				if i not in done:
-					done.append(i)
-					i = 'https:%s' % i 
-					print i
-					if len(sys.argv) == 3:
-						thread = Download(i, sys.argv[2])
-						thread.start()
-						threads.append(thread)
-					else:
-						thread = Download(i)
-						thread.start()
-						threads.append(thread)
+        for i in list:
+            if i not in done:
+                done.append(i)
+                i = 'https:%s' % i 
+                print i
+                if len(sys.argv) == 3:
+                    thread = Download(i, sys.argv[2])
+                    thread.start()
+                    threads.append(thread)
+                else:
+                    thread = Download(i)
+                    thread.start()
+                    threads.append(thread)
 
-			for thread in threads:
-				thread.join()
+        for thread in threads:
+            thread.join()
 
-	else:
-		import os
-		print 'This is a script to download images from 4chan.'
-		print 'usage: %s http://server.com/ [directory]' % os.path.basename(sys.argv[0])
-		print 'usage: %s http://server.com/' % os.path.basename(sys.argv[0])
+    else:
+        import os
+        print 'This is a script to download images from 4chan.'
+        print 'usage: %s http://server.com/ [directory]' % os.path.basename(sys.argv[0])
+        print 'usage: %s http://server.com/' % os.path.basename(sys.argv[0])
